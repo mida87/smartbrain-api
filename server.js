a652bba8e934d99e486e9a905ece78d19e5ea36a
Martin Marecic <martin.marecic@gmail.com>
2019-09-03 15:26:46 +0200

    preparing DB connection for heroku

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image.js');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        port: '5433',
-        user : 'test',
-        password : 'test',
-        database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
